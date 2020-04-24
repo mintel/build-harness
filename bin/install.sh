617db9c51886ddbaea9a3e9bbe0a505f252116ab
Charlie <charlie@charliemathews.com>
2020-04-24 15:07:28 -0500

    Clone only the relevant commit w/ depth=1 on install (#37)
    * Implement @jtdoepke's suggestion to clone only the relevant commit w/ depth=1

--- bin/install.sh
+++ bin/install.sh
@@ -4,31 +4,22 @@ set -e
 export BUILD_HARNESS_ORG=${1:-mintel}
 export BUILD_HARNESS_PROJECT=${2:-build-harness}
 export BUILD_HARNESS_BRANCH=${3:-master}
-export BUILD_HARNESS_TAG=${4:-latest}
 export GITHUB_REPO="https://github.com/${BUILD_HARNESS_ORG}/${BUILD_HARNESS_PROJECT}.git"
 
-echo "Installing ${BUILD_HARNESS_PROJECT}..."
-
-if [ "$BUILD_HARNESS_PROJECT" ] && [ -d "$BUILD_HARNESS_PROJECT" ]; then
-  echo "Updating from ${GITHUB_REPO}#${BUILD_HARNESS_BRANCH}"
-  (
-    cd $BUILD_HARNESS_PROJECT && \
-    git fetch origin --tags --force && \
-    git checkout $BUILD_HARNESS_BRANCH >/dev/null && \
-    git pull \
-  )
-else
-  echo "Cloning from ${GITHUB_REPO}#${BUILD_HARNESS_BRANCH}"
-  git clone -b $BUILD_HARNESS_BRANCH $GITHUB_REPO >/dev/null 2>&1
+if [ -z "$BUILD_HARNESS_BRANCH" ] || [ "$BUILD_HARNESS_BRANCH" == "master" ] ; then
+  BUILD_HARNESS_BRANCH="$(git ls-remote --tags "$GITHUB_REPO" 2>/dev/null | cut -d/ -f3 | sort -rV | head -1)"
 fi
 
+echo "§ ${GITHUB_REPO}@${BUILD_HARNESS_BRANCH}"
+
 if [ "$BUILD_HARNESS_PROJECT" ] && [ -d "$BUILD_HARNESS_PROJECT" ]; then
+  echo "Fetching..."
   cd $BUILD_HARNESS_PROJECT
-  BUILD_HARNESS_LATEST_TAG=$(git describe --abbrev=0 --tags)
-  if [ "$BUILD_HARNESS_TAG" == "latest" ]; then
-    git reset --hard $BUILD_HARNESS_LATEST_TAG;
-  else
-    git reset --hard $BUILD_HARNESS_TAG
-  fi
-  echo "Using ${BUILD_HARNESS_PROJECT}/${BUILD_HARNESS_BRANCH}@$(git tag --points-at HEAD | paste -sd ',' -)"
+  git fetch --depth=1 --no-tags origin "${BUILD_HARNESS_BRANCH}" 2>/dev/null
+  git checkout FETCH_HEAD 2>/dev/null
+else
+  echo "Cloning..."
+  git clone -b "${BUILD_HARNESS_BRANCH}" --depth=1 --no-tags "${GITHUB_REPO}" 2>/dev/null
 fi
+
+echo "Done."
